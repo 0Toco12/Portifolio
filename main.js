0702e9529fdb1ac65cc11255377f087245b9a4f0
Gui Sanches <guilherme.sanches.mag@gmail.com>
2024-07-10 17:52:16 -0300

    Quase lá

---
+++ main.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     setTimeout(function() {
+//         document.getElementById("opening-page").style.opacity = 0;
+//         setTimeout(function() {
+//             document.getElementById("opening-page").style.display = "none";
+//             document.getElementById("s1").style.display="flex";
+//         }, 1000); // Tempo de transição (1 segundo)
+//     }, 3000); // Tempo de exibição da tela de abertura (3 segundos)
+// });
+
+
