a1da004503adc49ac69b63d33f9ab74cc203cc13
Gui Sanches <guilherme.sanches.mag@gmail.com>
2024-08-20 15:40:10 -0300

    Terminando

--- main.js
+++ main.js
@@ -1,32 +1,17 @@
-const letters = document.querySelectorAll('.letter');
-const svg = document.getElementById("svg");
 const s1 = document.getElementById("s1");
-const s2 = document.getElementById("s2");
-const opening = document.getElementById("opening-page");
+const s3 = document.getElementById("s3");
+const intro = document.getElementById("intro");
 
-letters.forEach((letter, index) => {
-    letter.style.strokeDasharray = `${letter.getTotalLength()} 1000`;
-    letter.style.strokeDashoffset = '1000';
+document.addEventListener("DOMContentLoaded", function() {
+    function showMainContent() {
+        intro.classList.add('hidden');
+        setTimeout(function() {
+            intro.style.display = 'none';
+            document.getElementById('main-content').style.display = 'block';
+            s1.style.display = "flex";
+            s3.style.display = "grid";
+        }, 2000);
+    }
+
+    setTimeout(showMainContent, 6000);
 });
-
-setTimeout(() => {
-    letters.forEach((letter) => {
-        letter.classList.remove('hidden');
-        letter.classList.add('animated');
-    });
-    setTimeout(() => {
-        letters.forEach((letter) => {
-            letter.style.fill = "#F7F1BA"
-        });
-        setTimeout(() => {
-            opening.classList.add('fade-out'); // adiciona a classe de fade-out
-            s1.classList.add('aparecendo');
-            s2.classList.add('aparecendo');
-            setTimeout(() => {
-                opening.style.display = "none";
-                s1.style.display = "flex";
-                s2.style.display = "grid";
-            }, 1000); // espera 1 segundo para esconder o elemento
-        }, 9000); // espera 9 segundos para iniciar a transição
-    }, 7000);
-}, 1000);
