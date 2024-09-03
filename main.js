11c1871bc2b02f248d63824b6c8110a828e54823
Gui Sanches <guilherme.sanches.mag@gmail.com>
2024-09-03 14:49:12 -0300

    .

--- main.js
+++ main.js
@@ -15,3 +15,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
     setTimeout(showMainContent, 6000);
 });
+
+
+    // Obter o modal
+    var modal = document.getElementById("myModal");
+
+    // Obter o botão que abre o modal
+    var btn = document.getElementById("certificados");
+
+    // Obter o elemento <span> que fecha o modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // Quando o usuário clicar no botão, abre o modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+        s1.style.backdropFilter = 'blur(10px)';
+        s1.style.webkitBackdropFilter = 'blur(10px)';
+        s3.style.backdropFilter = 'blur(10px)';
+        s3.style.webkitBackdropFilter = 'blur(10px)';
+    }
+
+    // Quando o usuário clicar no <span> (x), fecha o modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Quando o usuário clicar em qualquer lugar fora do modal, fecha o modal
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
